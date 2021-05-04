5a0840f44e0238695cffc7dabec5a3d544fe028e
ittsya <ittsya@MacBook-Pro-ittsya.local>
2021-05-04 12:17:13 +0300

    Fix compiler warnings

--- include/screen.hpp
+++ include/screen.hpp
@@ -5,10 +5,16 @@
 #include <cinttypes>
 #include <utility>
 #include <vector>
+#include <bitset>
 #include <array>
 
 #define STD_SIZE_X 10
 #define STD_SIZE_Y 10
+
+#define WIDTH_AND_HEIGHT_BEYOND_EDGE 0
+#define WIDTH_BEYOND_EDGE  1
+#define HEIGHT_BEYOND_EDGE 2
+#define FN_OK 10
 
 namespace Screen
 {
@@ -33,54 +39,52 @@ namespace Screen
         std::uint_least32_t fgcolor_, bgcolor_;
         std::size_t         size_x_,  size_y_;
         std::size_t         pos_x,   pos_y;
+
+        
     
     protected:
     
-        explicit ScreenObjectC(): size_x_(STD_SIZE_X), size_y_(STD_SIZE_Y), fgcolor_(COLOR::WHITE), bgcolor_(COLOR::BLACK) {} 
-        explicit ScreenObjectC(size_t x, size_t y) : size_x_(x), size_y_(y), fgcolor_(COLOR::WHITE), bgcolor_(COLOR::BLACK) {} 
-        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor) : size_x_(x), size_y_(y), fgcolor_(fgcolor), bgcolor_(COLOR::BLACK) {} 
-        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor, std::uint_least32_t bgcolor) : size_x_(x), size_y_(y), fgcolor_(fgcolor), bgcolor_(bgcolor) {} 
-        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor, std::uint_least32_t bgcolor, std::size_t posx, std::size_t posy) : size_x_(x), size_y_(y), pos_x(posx), pos_y(posy), fgcolor_(fgcolor), bgcolor_(bgcolor) {} 
+        explicit ScreenObjectC(): fgcolor_(COLOR::WHITE), bgcolor_(COLOR::BLACK), size_x_(STD_SIZE_X), size_y_(STD_SIZE_Y)  {} 
+        explicit ScreenObjectC(size_t x, size_t y) : fgcolor_(COLOR::WHITE), bgcolor_(COLOR::BLACK), size_x_(x), size_y_(y) {} 
+        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor) : fgcolor_(fgcolor), size_x_(x), size_y_(y) {} 
+        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor, std::uint_least32_t bgcolor) : fgcolor_(fgcolor), bgcolor_(bgcolor), size_x_(x), size_y_(y) {} 
+        explicit ScreenObjectC(size_t x, size_t y, std::uint_least32_t fgcolor, std::uint_least32_t bgcolor, std::size_t posx, std::size_t posy) : fgcolor_(fgcolor), bgcolor_(bgcolor), size_x_(x), size_y_(y), pos_x(posx), pos_y(posy) {} 
     
     public:
         /* Swap foreground and background colors */
-        inline void reverse_colors(...); //__attribute__((__noreturn__, always_inline));
+        inline void reverse_colors() __attribute__((always_inline));
     
         /* Set color for the object */
         inline void Paint(
             std::uint_least32_t fgcolor,
-            std::uint_least32_t bgcolor,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            std::uint_least32_t bgcolor
+        ) __attribute__((always_inline));
     
         /* Set only foreground color for the object */
         inline void Paint(
-            std::uint_least32_t fgcolor,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            std::uint_least32_t fgcolor
+        ) __attribute__((always_inline));
         
         /*Set size for the object */
         inline void set_size(
             size_t x, 
-            size_t y,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            size_t y
+        ) __attribute__((always_inline));
     
         /*Set object x and y position */   
         inline void set_pos_xy(
             size_t x,
-            size_t y,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            size_t y
+        ) __attribute__((always_inline));
     
         /* Get current oject position in (x,y) format */
-        inline const std::pair<size_t, size_t> GetPosition(...) __attribute__((always_inline, const));
+        inline std::pair<size_t, size_t> GetPosition() const __attribute__((always_inline, const));
     
         /* Get object size in (x,y) format */
-        inline const std::pair<size_t, size_t> GetSize(...) __attribute__((always_inline, const));
+        inline std::pair<size_t, size_t> GetSize() const __attribute__((always_inline, const));
     
         /* Get object color in (x,y)format  */ 
-        inline const std::pair<std::uint_least32_t, std::uint_least32_t> GetColor(...) __attribute__((always_inline, const));
+        inline std::pair<std::uint_least32_t, std::uint_least32_t> GetColor() const __attribute__((always_inline, const));
     };
     
     /* Nice and shiny Cell class*/
@@ -89,8 +93,10 @@ namespace Screen
     
     private:   
     
-        char32_t            cell_symbol;    
+        char            cell_symbol = ' ';    
         std::array<bool, 7> cell_parameters; 
+
+        friend std::ostream &operator<<(std::ostream &output, const CellC &D);
     
     public:
     
@@ -116,24 +122,22 @@ namespace Screen
             *  | UNDERLINE  |      6           |
             *  |____________|__________________|  
             */         
-            std::array<bool, 7> in_params,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            std::array<bool, 7> in_params
+        ) __attribute__((always_inline));
         
         /* Set 32 bit char cahracter for cell */
         inline void set_character(
-            char32_t character,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            char chararacter
+        ) __attribute__((always_inline));
 
         /* Init Cell with none additional text styles, and with "?" symbol in it */
-        inline void init_default(...) __attribute__((__noreturn__, always_inline));
+        inline void init_default();
 
         /* Getter for cell params */
-        inline std::array<bool, 7> get_params(...) __attribute__((always_inline));
+        inline std::array<bool, 7> get_params() const __attribute__((always_inline));
         
         /* Getter for cell character */
-        inline char32_t get_character(...) __attribute__((always_inline)); 
+        inline char32_t get_character() const __attribute__((always_inline)); 
 
     };
     
@@ -168,18 +172,23 @@ namespace Screen
     
     public:
 
-        explicit Window(uint8_t ScreenW, uint8_t ScreenH) : CellWidth(10), CellHeight(15), ScreenWidth(ScreenW), ScreenHeight(ScreenH) {}
-        explicit Window(uint8_t CellW, uint8_t CellH, uint8_t ScreenW, uint8_t ScreenH) : CellWidth(CellW), CellHeight(CellH), ScreenWidth(ScreenW), ScreenHeight(ScreenH) {}
+        explicit Window(uint32_t ScreenW, uint32_t ScreenH) : CellWidth(10), CellHeight(15), ScreenWidth(ScreenW), ScreenHeight(ScreenH) {}
+        explicit Window(uint32_t CellW, uint32_t CellH, uint32_t ScreenW, uint32_t ScreenH) : CellWidth(CellW), CellHeight(CellH), ScreenWidth(ScreenW), ScreenHeight(ScreenH) {}
         
         /* Set cursor coordinates */
         inline void SetCurCoordinates(
             size_t x,
-            size_t y,
-            ...
-        ) __attribute__((__noreturn__, always_inline));
+            size_t y
+        ) __attribute__((always_inline));
 
         /* Make grid of cells */
-        void MakeCellGrid(...) __attribute__((__noreturn__, always_inline));
+        void MakeCellGrid(uint32_t columns, uint32_t rows);
+        
+        /* Check if cells are fitting in screen space */
+        uint32_t CellsFits(uint32_t rows, uint32_t columns);
+
+        /* Fix size of cells*/
+        void FixCellSize();
 
     };
 
@@ -188,101 +197,149 @@ namespace Screen
     /*|-------------------------------------------------|*/
     
             /*SCREEN OBJECT CLASS FUNCTIONS */
-    inline void ScreenObjectC::reverse_colors(...)
+    inline void ScreenObjectC::reverse_colors()
     {
         std::swap(this->bgcolor_, this->fgcolor_);
     }
 
-    inline void ScreenObjectC::Paint(std::uint_least32_t fgcolor, std::uint_least32_t bgcolor, ...)
+    inline void ScreenObjectC::Paint(std::uint_least32_t fgcolor, std::uint_least32_t bgcolor)
     {
         this->fgcolor_ = fgcolor; this->bgcolor_ = bgcolor;
     }
 
-    inline void ScreenObjectC::Paint(std::uint_least32_t fgcolor, ...)
+    inline void ScreenObjectC::Paint(std::uint_least32_t fgcolor)
     {
         this->fgcolor_ = fgcolor;
     }
 
-    inline void ScreenObjectC::set_size(size_t x, size_t y, ...)
+    inline void ScreenObjectC::set_size(size_t x, size_t y)
     {
         this->size_x_ = x; this->size_y_ = y;
     }
 
-    inline void ScreenObjectC::set_pos_xy(size_t x, size_t y, ...)
+    inline void ScreenObjectC::set_pos_xy(size_t x, size_t y)
     {
         this->pos_x = x; this->pos_y = y;
     }
 
-    inline const std::pair<size_t, size_t> ScreenObjectC::GetPosition(...)
+    inline std::pair<size_t, size_t>  ScreenObjectC::GetPosition() const
     {
         return std::make_pair(this->pos_x, this->pos_y);
     }
 
-    inline const std::pair<size_t, size_t> ScreenObjectC::GetSize(...)
+    inline std::pair<size_t, size_t>  ScreenObjectC::GetSize() const
     {
         return std::make_pair(this->size_x_, this->size_y_);
     }
 
-    inline const std::pair<std::uint_least32_t, std::uint_least32_t> ScreenObjectC::GetColor(...)
+    inline std::pair<std::uint_least32_t, std::uint_least32_t> ScreenObjectC::GetColor() const
     {
         return std::make_pair(this->fgcolor_, this->bgcolor_);
     }
 
     /*CELL CLASS FUNCTIONS*/
-    inline void CellC::set_params(std::array<bool, 7> in_params, ...)
+    inline void CellC::set_params(std::array<bool, 7> in_params)
     {
         for (int i = 0; i < 7; i++)
         {
             this->cell_parameters[i] = in_params[i];
         }     
     }
-    inline void CellC::set_character(char32_t character, ...)
+    inline void CellC::set_character(char character)
     {
         this->cell_symbol = character;
     }
 
-    inline void CellC::init_default(...)
+    void CellC::init_default()
     {
-        this->cell_symbol = U'?'; this->cell_parameters = {0, 0, 0, 0, 0, 0, 0};
+        this->cell_symbol = '?';
+        this->cell_parameters = {0, 0, 0, 0, 0, 0, 0};
     }
 
-    inline std::array<bool, 7> CellC::get_params(...)
+    inline std::array<bool, 7> CellC::get_params() const 
     {
         return this->cell_parameters;
     }
 
-    inline char32_t CellC::get_character(...)
+    inline char32_t CellC::get_character() const 
     {
         return this->cell_symbol;
     }
 
     /*WINDOW CLASS FUNCTION*/
-    inline void Window::SetCurCoordinates(size_t x, size_t y, ...)
+    inline void Window::SetCurCoordinates(size_t x, size_t y)
     {
         this->Cursor.x = x; this->Cursor.y = y;
     }
 
-    void Window::MakeCellGrid(...)
+    void Window::MakeCellGrid(uint32_t columns, uint32_t rows)
     {   
-        constexpr rows    = this->ScreenWidth / this->CellWidth;
-        constexpr columns = this->ScreenHeight / this->CellHeight
-        
-        std::array<CellC, columns> CellsInColumns;
+        std::vector<CellC> CellGrid;
 
-        // Set up delimeters
-        uint8_t h_del;
-        uint8_t w_del;
+        //Set up delimeters
+        uint32_t h_del;
+        uint32_t w_del;
         
-        for(uint8_t k; k < rows ; ++k)
+        CellC Cell; 
+        
+        if(CellsFits(columns, rows) == FN_OK) {FixCellSize();} 
+        
+        for(uint32_t k = 0; k < rows; k++)
         {
-            for(uint8_t m; m < columns; ++m)
+            w_del = this->ScreenHeight * k;
+            for(uint32_t m = 0; m < columns; m++)
             {
-               CellsInColumns[m] = CellC(this->CellHeight, this->CellWidth, COLOR::WHITE, COLOR::BLACK, w_del, h_del);     
                h_del = this->ScreenWidth  * m;
-               w_del = this->ScreenHeight * k;
+               fprintf(stderr, "%d", h_del);
+               Cell = CellC(this->CellHeight, this->CellWidth, COLOR::WHITE, COLOR::BLACK, w_del, h_del);     
+               Cell.init_default();
+               std::cout << Cell << std::endl;
+               CellGrid.push_back(Cell); 
+               
             }
-            this->cells[k].push_back(CellsInColumns);
+            this->cells.push_back(CellGrid);
+            CellGrid.clear();
+            
         }
+
+    }
+
+    uint32_t Window::CellsFits(uint32_t rows, uint32_t columns)
+    {
+        if(((rows * this->CellWidth) > this->ScreenWidth) && ((columns * this->CellHeight) > this->ScreenHeight)) {return WIDTH_AND_HEIGHT_BEYOND_EDGE;}
+        if((rows * this->CellWidth) > this->ScreenWidth)      {return WIDTH_BEYOND_EDGE;}
+        if((columns * this->CellHeight) > this->ScreenHeight) {return HEIGHT_BEYOND_EDGE;}      
+        return FN_OK;
+    }
+
+    void Window::FixCellSize()
+    {
+        fprintf(stderr, "Size of cells are incorrect, fitting them in...");
+        while (true)
+        {
+            switch(CellsFits(this->ScreenWidth  / this->CellWidth, this->ScreenHeight / this->CellHeight))
+            {
+                case WIDTH_BEYOND_EDGE:
+                    this->CellWidth  -=1; break;
+                case HEIGHT_BEYOND_EDGE:
+                    this->CellHeight -=1; break;
+                case WIDTH_AND_HEIGHT_BEYOND_EDGE:
+                    this->CellWidth  -=1; this->CellHeight -=1; break;
+            } 
+            if(CellsFits(ScreenWidth / CellWidth ,ScreenHeight / CellHeight) == FN_OK)
+                fprintf(stderr, "\nFixed! Cell height : %d   Cell width : %d\n", this->CellHeight, this->CellWidth);
+                break;
+            
+        }
+    }
+    std::ostream &operator<<(std::ostream &output, const CellC &D)
+    {
+        output << "ScreenObject class in " << &D << ":\n";
+        output << "Color:    (" << std::hex << D.fgcolor_  << ", " << D.bgcolor_ << ")  ";
+        output << "Position: (" << std::dec << D.pos_x     << ", " << D.pos_y    << ")  ";
+        output << "Size:     (" << D.size_x_ << ", "<< D.size_y_ << ")\n";
+        output << "Character: " << D.cell_symbol;
+        return output;
     }
 }
 #endif /* SCREEN_H */
