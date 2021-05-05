cc945d67dafd57718faa1be95a1e85d5ae3aa676
ittsya <ittsya@MacBook-Pro-ittsya.local>
2021-05-05 21:16:23 +0300

    better architecture

--- src/rendering/SDL2_.cpp
+++ src/rendering/SDL2_.cpp
@@ -4,36 +4,38 @@ struct pixel
     uint8_t a,r,g,b;
 };
 
-void SDL_ReInit(SLD_Window &wnd,SDL_Texture &texture, SDL_Renderer &renderer, Screen::Window &WindowClass)
+void SDL_ReInit(SDL_Window *window, SDL_Texture *texture, SDL_Renderer *renderer, Screen::Window *WindowClass)
 {
     const Int WindowHeight = WindowClass->GetWndCoords().first;
     const Int WindowWidth  = WindowClass->GetWndCoords().second;
 
-    if(!*window)
+    if(window == nullptr)
     {
-    *wnd = SDL_CreateWindow("Terminal", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    window = SDL_CreateWindow("Terminal", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                             WindowWidth, WindowHeight, 0);
     }
     else
     {
-        SDL_SetWindowSize(*window, WindowWidth, WindowHeight);
+        SDL_SetWindowSize(window, WindowWidth, WindowHeight);
     }
-    if(!*renderer)
+    if(!renderer)
     {
-        *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDER_ACCELERATED);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     }
-    if(!*texture)
+    if(!texture)
     {
-        *texture = SDL_CreateTexture(*renderer, SDL_PIXELFORMAT_ARGB8888,
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                                       SDL_TEXTUREACCESS_STATIC,
                                       WindowWidth, WindowHeight);
     }
+    
+
+
 
 }
-
-void SDL_Destroy(SLD_Window &wnd, SLD_Renderer renderer, SDL_Texture texture)
+void SDL_Destroy(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture)
 {
-    SDL_DestroyRenderer(*renderer);
-    SDL_DestroyTexture(*texture);
-    SDL_DestroyWindow(*wnd);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
 }
