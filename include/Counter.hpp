37445e346a4bc777520db4d3314cd4890b186768
ittsya <ittsya@gmail.com>
2021-05-13 03:43:02 +0300

    Rewrite with c++20 features

--- include/Counter.hpp
+++ include/Counter.hpp
@@ -4,33 +4,23 @@
 
 #include <type_traits>
 
-#define ZERO (_TYPE)0
-#define ZERO_f(x) (_TYPE)x
+#include "Concepts.hpp"
 
-template<typename T>
-concept Countable =
-    !std::is_same<T, unsigned char>::value &&
-    !std::is_same<T, char>::value &&
-    std::is_arithmetic<T>::value;
-
-template<Countable _TYPE = signed int>
-struct Counter {
+template<_Countable_v _TYPE = signed int> struct Counter
+{
 private:
     _TYPE _COUNTER, _STARTVALUE;
 public:
 
-    inline void set(_TYPE StartValue = ZERO) {this->_STARTVALUE = StartValue;}
+    inline void set(_TYPE StartValue = _TYPE()) {this->_STARTVALUE = StartValue;}
     
     inline void reset() {this->_STARTVALUE = this->_STARTVALUE;}
     
-    inline void start(_TYPE StartNum = ZERO) {this->_COUNTER = StartNum; }
+    inline void start(_TYPE StartNum = _TYPE()) {this->_COUNTER = StartNum; }
 
-    inline void tick(_TYPE how_much = ZERO + ZERO_f(1)) {this->_COUNTER += how_much;}
+    inline void tick(_TYPE how_much = _TYPE() + (_TYPE)1) {this->_COUNTER += how_much;}
 
     _TYPE GetCount() {return this->_COUNTER;}
-
 };
 
-#undef ZERO
-#undef ZERO_f
 #endif //PROJECT_COUNTER_HPP
